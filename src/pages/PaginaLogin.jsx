de5456bbabcc7a2df21a48419d2185fcfb7e82ea
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-05-06 09:33:06 -0300

    inicio

---
+++ src/pages/PaginaLogin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/paginaLogin.css"
+
+export default function PaginaLogin(){
+    return(
+        <div>
+            <div className="retanguloAzul">
+                <h1>Login</h1>
+            </div>
+        </div>
+    )
+}

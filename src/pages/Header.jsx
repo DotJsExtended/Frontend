62294470d17eab508d66a26c591293bea9589da3
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-05-13 10:40:17 -0300

    ./Frontend/public e /styles - Header terminado

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,12 +4,15 @@ export default function Header()
 {
     return (
         <div className="Header">
-        <div className="titleHeader">
-            Freelab
-        </div>
-        <div className="subtitleHeader">
-            Cadastre-se e junte-se à nossa comunidade de talentosos freelancers e contratantes
-        </div>
+            <div className="titleHeader">
+                Freelab
+            </div>
+            <div className="subtitleHeader2">
+                <div className="subtitleHeader">
+                    Cadastre-se e junte-se à nossa comunidade de talentosos freelancers e contratantes
+                </div>
+            </div>
+                
         </div>
     )
 }

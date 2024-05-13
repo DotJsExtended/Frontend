92360bbfa1b56797a66ddc0e87471ac118c18f2d
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-05-13 09:53:53 -0300

    ./Frontend - Começo

---
+++ src/pages/Cadastro.jsx
@@ -0,0 +1,9 @@
+import Header from "./Header.jsx"
+
+export default function Cadastro() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
---
+++ src/pages/Header.jsx
@@ -0,0 +1,15 @@
+import '../styles/Header.css'
+
+export default function Header()
+{
+    return (
+        <div className="Header">
+        <div className="titleHeader">
+            Freelab
+        </div>
+        <div className="subtitleHeader">
+            Cadastre-se e junte-se à nossa comunidade de talentosos freelancers e contratantes
+        </div>
+        </div>
+    )
+}

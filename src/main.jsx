9d12819af2a89e7a4040e7b57eed4d2a06445d96
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-05-16 09:30:28 -0300

    Imports main alterados

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import PaginaLogin from './pages/PaginaLogin'
-import PaginaHome from './pages/PaginaHome'
 import Rotas from './Rotas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

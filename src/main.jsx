8db78174fd7bca6775ccb45c6c561416c2bfc8e9
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-05-15 21:09:44 -0300

    oi

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PaginaSobreNos from './pages/PaginaSobreNos'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <PaginaSobreNos/>
-  </React.StrictMode>,
-)

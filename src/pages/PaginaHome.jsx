32c2a49e3205b51e4190c85c65a1d40f4218a407
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-05-15 21:15:44 -0300

    foi?

---
+++ src/pages/PaginaHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../components/Logo';
+
+export default function PaginaHome() {
+    return (
+        <>
+            <div className='Paginahome'>
+            <Logo className='Logo'/>
+            </div>
+        </>
+            )
+}

ef7c152c2bc09c543e5348ebc81bd8c60ba2cb42
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-05-15 22:33:10 -0300

    quase merge 2

--- src/components/HeaderNicolas.jsx
+++ src/components/HeaderNicolas.jsx
@@ -1,19 +1,19 @@
 export default function Header({ texto1, texto2 }) {
-    return (
-     <header>
-        <div className="headertexto">
-        <div className="logo">FreeLab</div>
-        <div className="textos">
-        <h3>{texto1}</h3>
-        <h3>{texto2}</h3>
-        </div>
-        </div>
-          <button className="login">
-          LOG-IN
-          </button>
-          <button className="criarconta">
-          CRIAR CONTA
-          </button>
-       </header>
-      )
-  }
+  return (
+   <header>
+      <div className="headertexto">
+      <div className="logo">FreeLab</div>
+      <div className="textos">
+      <h3>{texto1}</h3>
+      <h3>{texto2}</h3>
+      </div>
+      </div>
+        <button className="login">
+        LOG-IN
+        </button>
+        <button className="criarconta">
+        CRIAR CONTA
+        </button>
+     </header>
+    )
+}

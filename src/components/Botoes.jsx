e8653ddd8b643991b07270563b0bb9ca3ae72df7
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-05-15 22:31:25 -0300

    quase merge

---
+++ src/components/Botoes.jsx
@@ -0,0 +1,12 @@
+export default function Button() {
+    return (
+      <section>
+      <button className="button1">
+      QUERO CONTRATAR
+      </button>
+      <button className="button2">
+      QUERO TRABALHAR
+      </button>
+      </section>
+     );
+}
--- src/components/HeaderNicolas.jsx
+++ src/components/HeaderNicolas.jsx
@@ -1,23 +1,19 @@
-import Button from "./Buttons/Button"
-
-export default function Header({ label, texto1, texto2 }) {
-  return (
-   <header>
-      <div className="headertexto">
-      <div className="logo">FreeLab</div>
-      <h3>{texto1}</h3>
-      <h3>{texto2}</h3>
-      </div>
-      <Button
-                className={"login"}
-                textobtt={"LOG-IN"} 
-                caminho={"/login"}
-            />
-      <Button
-                className={"criarconta"}
-                textobtt={"CRIAR CONTA"} 
-                caminho={"/cadastro"}
-            />
-     </header>
-    )
-}
+export default function Header({ texto1, texto2 }) {
+    return (
+     <header>
+        <div className="headertexto">
+        <div className="logo">FreeLab</div>
+        <div className="textos">
+        <h3>{texto1}</h3>
+        <h3>{texto2}</h3>
+        </div>
+        </div>
+          <button className="login">
+          LOG-IN
+          </button>
+          <button className="criarconta">
+          CRIAR CONTA
+          </button>
+       </header>
+      )
+  }

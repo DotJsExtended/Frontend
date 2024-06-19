e66d595da2732cbb6f2bd38cad9232c34aa001c8
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-06-19 10:45:54 -0300

    Visualização da pagina de recuperação de senha corrigida

--- src/pages/PaginaRecuperarSenha/PaginaRecuperaSenha.jsx
+++ src/pages/PaginaRecuperarSenha/PaginaRecuperaSenha.jsx
@@ -18,7 +18,7 @@ export default function Pagina2FAtelefone() {
             </div>
 
             <ImageStandard imagem={"recuperarSenhaImg.svg"} className={"ImgRecSenha"} />
-            <ButtonTxt className={"botaoContinuarSeta"} texto={"CONTINUAR"} img={"arrowContinuar.svg"} caminho={"/home"} />
+            <ButtonTxt className={"botaoContinuarSeta"} texto={"CONTINUAR"} img={"arrowContinuar.svg"} caminho={"/recuperar-senha-confirmacao"} />
 
         </>
     )

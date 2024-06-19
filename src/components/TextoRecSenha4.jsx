23c7d53156aadfc76c88a61837583d3637800496
maduveridiano <dudqaa13@gmail.com>
2024-06-18 22:11:17 -0300

    chore: Refactor TextoRecSenha4 component and styles

--- src/components/TextoRecSenha4.jsx
+++ src/components/TextoRecSenha4.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function TextoRecSenha4() {
     return (
         <div className="textoRecSenha4">
-            <div className="ImgAvião">
-        <img src="./../imagerec.svg"/>
-            </div>
+        <img className="ImgAvião" src="./../imagerec.svg"/>  
             <div className="textaviso">
                 <p>O e-mail para a redefinição de senha foi enviado a você! Verifique sua caixa de entrada e siga o passo a passo.</p>
             </div>

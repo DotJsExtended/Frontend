02c213e644f8955c0a0c615a068aa479d891e64f
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-09-07 20:24:37 -0300

    Página de boas vindas consertada

--- src/pages/PaginaBoasVindas/PaginaBoasVindas.jsx
+++ src/pages/PaginaBoasVindas/PaginaBoasVindas.jsx
@@ -6,31 +6,33 @@ function PaginaBoasVindas() {
 
   return (
     <>
-      <Header
-        texto1={'SOBRE NÓS'} texto2={'SAIBA MAIS'}
-      />
+      <Header/>
       <div className="sessao">
         <div className="texto">
-          <h1>Transforme Ideias</h1>
-          <h1>em Realidade -</h1>
+          <div>
+            <h1>Transforme Ideias</h1>
+            <h1>em Realidade -</h1>
+          </div>
+
           <h3>Contrate talentos flexíveis hoje mesmo!</h3>
           <p>Conectamos talentos excepcionais a projetos extraordinários. Descubra uma comunidade vibrante de freelancers qualificados prontos para transformar suas ideias em realidade.</p>
-          <Button
-            textobtt={'QUERO CONTRATAR'}
-            className={'botaoContratar'}
-            caminho={"/cadastro"}
-          />
-          <Button
-            textobtt={'QUERO TRABALHAR'}  
-            className={'botaoTrabalhar'}
-            caminho={"/cadastro"}
-          />
+          <div className='botoesCadastrarBoasVindas'>
+            <Button
+              textobtt={'QUERO CONTRATAR'}
+              className={'botaoContratar'}
+              caminho={"/cadastro"}
+            />
+            <Button
+              textobtt={'QUERO TRABALHAR'}
+              className={'botaoTrabalhar'}
+              caminho={"/cadastro"}
+            />
+          </div>
+
         </div>
 
         <div className='imagemComposta'>
-          <div className="fundo">
-            <img src="/fundo.png"></img>
-          </div>
+
           <div className="boneco">
             <img src="/boneco.png"></img>
           </div>

f492a577ab502fcecde53f28c514e22af440a5ee
schddan <danielschmitzsc@gmail.com>
2024-09-11 12:14:41 -0300

    Mensagem de boa noite adicionada

--- src/components/SaudacoesPosLogin/SaudacoesPosLogin.jsx
+++ src/components/SaudacoesPosLogin/SaudacoesPosLogin.jsx
@@ -6,8 +6,10 @@ export default function SaudacoesPosLogin({apelido, fotoPerfil}){
     let saudacao;
     if(hour < 12)
         saudacao = "Bom dia"
-    else
+    else if (hour > 12 && hour < 18)
         saudacao = "Boa tarde"
+    else
+        saudacao = "Boa noite"
     return(
         <div className="saudacoesPosLogin">
             <p>{saudacao}, {apelido}!</p>
